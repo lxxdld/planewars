5c2cc06774d6e09325b331f934dc4e856654ca02
brian <296526598@qq.com>
2019-05-27 21:12:04 +0800

    second ci

--- planewars/ship.py
+++ planewars/ship.py
@@ -15,9 +15,9 @@ class Ship():
 		self.moving_right = False
 		self.moving_left = False
 	def update(self):
-		if self.moving_right:
+		if self.moving_right and self.rect.right<self.screen_rect.right:
 			self.center +=self.ai_settings.ship_speed_factor
-		if self.moving_left:
+		if self.moving_left and self.rect.left>self.screen_rect.right:
 			self.center -=self.ai_settings.ship_speed_factor
 		#update target accordding to self.center
 		self.rect.centerx = self.center
